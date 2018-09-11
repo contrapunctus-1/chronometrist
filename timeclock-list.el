886f4a9164e3d6c43f0aa7245ec7bc7f2307a8e8
Kashish Sharma <music.kashish@gmail.com>
2018-09-11 18:09:38 +0530

    Organize file

--- timeclock-list.el
+++ timeclock-list.el
@@ -45,6 +45,7 @@
 ;; 2. timeclock-project-list contains only the projects found in the
 ;;    timeclock-file - no way for a user to specify tasks beforehand.
 
+;; IDLE TIMER
 (defun tclist/buffer-exists? (buffer-name)
   (--> (buffer-list)
        (mapcar #'buffer-name it)
@@ -82,28 +83,11 @@
     (with-current-buffer timeclock-list-buffer-name
       (tabulated-list-print t t))))
 
-(define-derived-mode timeclock-list-mode tabulated-list-mode "Timeclock-List"
-  "Major mode for timeclock-list."
-  (timeclock-reread-log)
-
-  (make-local-variable 'tabulated-list-format)
-  (setq tabulated-list-format [("Project" 25 t) ("Time" 10 t) ("Active" 3 t)])
-
-  (make-local-variable 'tabulated-list-entries)
-  (setq tabulated-list-entries 'tclist/entries)
-
-  (make-local-variable 'tabulated-list-sort-key)
-  (setq tabulated-list-sort-key '("Project" . nil))
-
-  (tabulated-list-init-header)
-
-  (run-with-idle-timer 3 t #'tclist/timer-fn)
-  (define-key timeclock-list-mode-map (kbd "RET") 'tclist/toggle-project)
-  (define-key timeclock-list-mode-map (kbd "l") 'tclist/open-timeclock-file))
-
+;; VARIABLES
 (defvar time-re "[0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\}")
 (defvar timeclock-list-buffer-name "*Timeclock-List*")
 
+;; FUNCTIONS
 (defun tclist/current-project ()
   "Returns the name of the currently clocked-in project, or nil
  if the user is not clocked in."
@@ -119,79 +103,6 @@
 (defun tclist/project-active? (project)
   "Returns t if PROJECT is currently clocked in, else nil."
   (equal (tclist/current-project) project))
-
-(defun tclist/toggle-project ()
-  "In a `timeclock-list' buffer, start or stop the project at point."
-  (interactive)
-  (let ((project-at-point (progn
-                            (beginning-of-line)
-                            (--> (buffer-substring-no-properties
-                                  (point)
-                                  (progn
-                                    (end-of-line)
-                                    (re-search-backward time-re nil t)))
-                                 (replace-regexp-in-string "[ \t]*$" "" it))))
-        (current-project  (tclist/current-project)))
-    ;; When changing projects/clocking in, suggest the project at point
-    (cl-letf (((symbol-function 'timeclock-ask-for-project)
-               (lambda ()
-                 (timeclock-completing-read
-                  (format "Clock into which project (default %s): "
-                          project-at-point)
-                  (mapcar 'list timeclock-project-list)
-                  project-at-point))))
-      ;; If we're clocked in to anything - clock out or change projects
-      (if current-project
-          (if (equal project-at-point current-project)
-              (timeclock-out nil nil t)
-            ;; We don't use timeclock-change because it doesn't prompt for the reason
-            (progn
-              (timeclock-out nil nil t)
-              (timeclock-in nil nil t)))
-        ;; Otherwise, run timeclock-in with project at point as default
-        ;; suggestion
-        (timeclock-in nil nil t)))
-    (timeclock-reread-log) ;; required when we create a new activity
-    ;; Trying to update partially doesn't update the activity indicator. Why?
-    (tabulated-list-print t nil)))
-
-(defun tclist/open-timeclock-file ()
-  (interactive)
-  (find-file-other-window timeclock-file)
-  (goto-char (point-max)))
-
-(defun timeclock-list ()
-  "Displays a list of the user's timeclock.el projects and the
-time spent on each today, based on their timelog file
-`timeclock-file'. The user can hit RET to start/stop projects.
-This is the 'listing command' for timeclock-list-mode."
-  (interactive)
-  (let ((buffer (get-buffer-create timeclock-list-buffer-name)))
-    ;; if we're in a timeclock-list buffer, kill it
-    ;; FIXME - kill buffer if it's visible, too
-    (if (and (equal (buffer-name) (buffer-name buffer))
-             (equal major-mode 'timeclock-list-mode))
-        (kill-this-buffer)
-      ;; otherwise create the buffer
-      (with-current-buffer buffer
-        (setq buffer-read-only nil)
-        (timeclock-list-mode)
-        (tabulated-list-print)
-        ;; using switch-to-buffer instead until we can preserve
-        ;; position of point across successive calls to timeclock-list
-        ;; (switch-to-buffer-other-window buffer)
-        (switch-to-buffer buffer)))))
-
-(defun tclist/entries ()
-  "Creates entries to be displayed in the buffer created by
-`timeclock-list'."
-  (mapcar (lambda (project-name)
-            (list project-name
-                  (vector project-name
-                          (tclist/project-time-one-day project-name)
-                          (if (tclist/project-active? project-name)
-                              "*" ""))))
-          timeclock-project-list))
 
 (defun tclist/timestamp->seconds (date-time)
   "Converts a timestamp to seconds since 00:00"
@@ -260,38 +171,98 @@ This is the 'listing command' for timeclock-list-mode."
                     ":"
                     (format "%02d" time-s))))))))
 
-(defun tclist/buffer-visible? (buffer-or-buffer-name)
-  "Returns t if BUFFER-OR-BUFFER-NAME is visible to user."
-  (-->
-   ;; It'd be simpler to start with this, but because it is possible
-   ;; that a frame partially covers another, and that frame has a
-   ;; buffer visible to the user; thus, using only the current frame
-   ;; isn't robust.
-   ;; (selected-frame)
-   ;; (window-list it)
-   (visible-frame-list)
-   (mapcar #'window-list it)
-   (mapcar (lambda (list)
-             (mapcar #'window-buffer list))
-           it)
-   (mapcar (lambda (list)
-             (mapcar (lambda (buffer)
-                       (if (bufferp buffer-or-buffer-name)
-                           (equal buffer-or-buffer-name buffer)
-                         (equal (buffer-name buffer)
-                                buffer-or-buffer-name)))
-                     list))
-           it)
-   (mapcar (lambda (list)
-             (seq-filter #'identity list))
-           it)
-   (if it t nil)))
+(defun tclist/entries ()
+  "Creates entries to be displayed in the buffer created by
+`timeclock-list'."
+  (mapcar (lambda (project-name)
+            (list project-name
+                  (vector project-name
+                          (tclist/project-time-one-day project-name)
+                          (if (tclist/project-active? project-name)
+                              "*" ""))))
+          timeclock-project-list))
 
-(defun tclist/timer-fn ()
-  (when (tclist/buffer-visible? timeclock-list-buffer-name)
-    (with-current-buffer timeclock-list-buffer-name
-      (tabulated-list-print t t))))
+;; MAJOR MODE
+(define-derived-mode timeclock-list-mode tabulated-list-mode "Timeclock-List"
+  "Major mode for timeclock-list."
+  (timeclock-reread-log)
 
-(run-with-idle-timer 3 t #'tclist/timer-fn)
+  (make-local-variable 'tabulated-list-format)
+  (setq tabulated-list-format [("Project" 25 t) ("Time" 10 t) ("Active" 3 t)])
+
+  (make-local-variable 'tabulated-list-entries)
+  (setq tabulated-list-entries 'tclist/entries)
+
+  (make-local-variable 'tabulated-list-sort-key)
+  (setq tabulated-list-sort-key '("Project" . nil))
+
+  (tabulated-list-init-header)
+
+  (run-with-idle-timer 3 t #'tclist/timer-fn)
+  (define-key timeclock-list-mode-map (kbd "RET") 'tclist/toggle-project)
+  (define-key timeclock-list-mode-map (kbd "l") 'tclist/open-timeclock-file))
+
+;; COMMANDS
+(defun tclist/toggle-project ()
+  "In a `timeclock-list' buffer, start or stop the project at point."
+  (interactive)
+  (let ((project-at-point (progn
+                            (beginning-of-line)
+                            (--> (buffer-substring-no-properties
+                                  (point)
+                                  (progn
+                                    (end-of-line)
+                                    (re-search-backward time-re nil t)))
+                                 (replace-regexp-in-string "[ \t]*$" "" it))))
+        (current-project  (tclist/current-project)))
+    ;; When changing projects/clocking in, suggest the project at point
+    (cl-letf (((symbol-function 'timeclock-ask-for-project)
+               (lambda ()
+                 (timeclock-completing-read
+                  (format "Clock into which project (default %s): "
+                          project-at-point)
+                  (mapcar 'list timeclock-project-list)
+                  project-at-point))))
+      ;; If we're clocked in to anything - clock out or change projects
+      (if current-project
+          (if (equal project-at-point current-project)
+              (timeclock-out nil nil t)
+            ;; We don't use timeclock-change because it doesn't prompt for the reason
+            (progn
+              (timeclock-out nil nil t)
+              (timeclock-in nil nil t)))
+        ;; Otherwise, run timeclock-in with project at point as default
+        ;; suggestion
+        (timeclock-in nil nil t)))
+    (timeclock-reread-log) ;; required when we create a new activity
+    ;; Trying to update partially doesn't update the activity indicator. Why?
+    (tabulated-list-print t nil)))
+
+(defun tclist/open-timeclock-file ()
+  (interactive)
+  (find-file-other-window timeclock-file)
+  (goto-char (point-max)))
+
+(defun timeclock-list ()
+  "Displays a list of the user's timeclock.el projects and the
+time spent on each today, based on their timelog file
+`timeclock-file'. The user can hit RET to start/stop projects.
+This is the 'listing command' for timeclock-list-mode."
+  (interactive)
+  (let ((buffer (get-buffer-create timeclock-list-buffer-name)))
+    ;; if we're in a timeclock-list buffer, kill it
+    ;; FIXME - kill buffer if it's visible, too
+    (if (and (equal (buffer-name) (buffer-name buffer))
+             (equal major-mode 'timeclock-list-mode))
+        (kill-this-buffer)
+      ;; otherwise create the buffer
+      (with-current-buffer buffer
+        (setq buffer-read-only nil)
+        (timeclock-list-mode)
+        (tabulated-list-print)
+        ;; using switch-to-buffer instead until we can preserve
+        ;; position of point across successive calls to timeclock-list
+        ;; (switch-to-buffer-other-window buffer)
+        (switch-to-buffer buffer)))))
 
 (provide 'timeclock-list)
