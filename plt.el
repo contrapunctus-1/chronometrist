8958243f8e9d0fd195f8d5eacfe9b60850509ed5
contrapunctus <music.kashish@gmail.com>
2019-08-17 12:07:37 +0530

    Implement return value specifiers

--- plt.el
+++ plt.el
@@ -6,8 +6,14 @@
 GET <return value specifier> WHERE <plist specifier expression> ..."
   )
 
-;; Leave defined comparison functions/pattern matching out for the
-;; moment - just use `equal' and focus on the basics first.
+;; Leave defined comparison functions/pattern matching/regexp matching
+;; out for the moment - just use `equal' and focus on the basics
+;; first.
+
+;; REVIEW - multiple fields (i.e. list values) for GET are returned as
+;; a flat list containing only the values. In cases where the values
+;; are nil, they don't appear (because we remove all nil values from
+;; the result). Should these be plists too?
 (cl-defun plt-query-function (table &key get specifiers)
   (let ((keyword-list (seq-filter #'keywordp specifiers))
         (return))
@@ -19,15 +25,16 @@ GET <return value specifier> WHERE <plist specifier expression> ..."
                                     (equal (plist-get value keyword)
                                            (plist-get specifiers keyword))))
                           (-all-p #'identity))
-                 (nconc return
-                        (cond ((keywordp get)
-                               (list
-                                (plist-get value get)))
-                              ;; (listp nil) => t, so we use consp
-                              ((consp get)
-                               (--map (plist-get value it)
-                                      get))
-                              (t value)))))
+                 (->> (cond ((keywordp get)
+                             (list
+                              (plist-get value get)))
+                            ;; (listp nil) => t, so we use consp
+                            ((consp get)
+                             (--map (plist-get value it)
+                                    get))
+                            (t value))
+                      (append return)
+                      (setq return))))
              table)
     (seq-remove #'null return)))
 
@@ -60,17 +67,21 @@ GET <return value specifier> WHERE <plist specifier expression> ..."
                                                (opt " ")))
                                       ""
                                       event-string)))
-            (incf key-counter)
-            (puthash key-counter
-                     (append `(:code ,code :date ,date :time ,time)
-                             (when (equal code "i")
-                               `(:project ,project-or-comment))
-                             (when (and (equal code "o")
-                                        (stringp project-or-comment)
-                                        (not
-                                         (string= project-or-comment "")))
-                               `(:comment ,project-or-comment)))
-                     test-table))
+            (pcase code
+              ("i"
+               (incf key-counter)
+               (puthash key-counter
+                        `(:project ,project-or-comment :start-date ,date :start-time ,time)
+                        test-table))
+              ("o"
+               (--> (gethash key-counter test-table)
+                    (append it
+                            `(:stop-date ,date :stop-time ,time)
+                            (when (and (stringp project-or-comment)
+                                       (not
+                                        (string= project-or-comment "")))
+                              `(:comment ,project-or-comment)))
+                    (puthash key-counter it test-table)))))
           (forward-line)
           (goto-char (point-at-bol))))
       nil)))
