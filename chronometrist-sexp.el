f757dc31e635e403f87278b4aea8499a0bd9eff4
contrapunctus <music.kashish@gmail.com>
2019-09-05 07:36:10 +0530

    Implement clock-in and -out functions

--- chronometrist-sexp.el
+++ chronometrist-sexp.el
@@ -13,26 +13,7 @@
 
 (defvar chronometrist-current-backend 'timeclock)
 
-(defvar chronometrist-backends-alist
-  '((timeclock . (:in-function  timeclock-in
-                  :out-function timeclock-out
-                  :file         timeclock-file))
-    (sexp . (:in-function  chronometrist-in-sexp
-             :out-function chronometrist-out-sexp
-             :file         chronometrist-file))))
 
-;;;; compatibility with timeclock.el
-(defvar chronometrist-in-function 'timeclock-in
-  "The function called by `chronometrist-in' to do its work.
-
-It should accept the same arguments as `timeclock-in'.")
-
-(defvar chronometrist-out-function 'timeclock-out
-  "The function called by `chronometrist-out' to do its work.
-
-It should accept the same arguments as `timeclock-out'.")
-
-;; `call-interactively' doesn't work for programmatic use.
 
 ;; Calling as a regular function doesn't work for interactive use,
 ;; unless we duplicate behaviour from timeclock-in/out.
@@ -77,21 +58,47 @@ The actual function to be called is determined by `chronometrist-out-function'."
           keys)
     plist))
 
-(defun chronometrist-in-sexp (&optional arg project find-project)
+(defun chronometrist-in-sexp (task plist)
   "Add new time interval as an s-expression to `chronometrist-file'.
 
-ARG, PROJECT, and FIND-PROJECT are used as in `timeclock-in'."
+TASK is the name of the task, a string.
+
+PLIST is a property list containing any other information about
+this time interval that should be recorded."
   (let ((buffer (find-file-noselect chronometrist-file))
         (tags   (plist-get plist :tags)))
     (with-current-buffer buffer
       (goto-char (point-max))
-      (when (not (bolp))
-        (insert "\n"))
-      (plist-pp
-       (append `(:task ,task) (when tags `(:tags ,tags))
-               (chronometrist-plist-remove plist :tags)
-               `(:start ,(format-time-string "%FT%T%z")))
-       buffer))))
+      (insert "\n")
+      (plist-pp (append `(:task ,task) (when tags `(:tags ,tags))
+                        (chronometrist-plist-remove plist :tags)
+                        `(:start ,(format-time-string "%FT%T%z")))
+                buffer)
+      (save-buffer))))
+
+(defun chronometrist-delete-list (&optional arg)
+  (let ((point-1 (point)))
+    (forward-sexp (or arg 1))
+    (delete-region point-1 (point))))
+
+;; TODO - implement PLIST arg
+(defun chronometrist-out-sexp (&optional plist)
+  "Record current moment as stop time to last s-exp in `chronometrist-file'.
+
+PLIST is a property list containing any other information about
+this time interval that should be recorded."
+  (let ((buffer (find-file-noselect chronometrist-file)))
+    (with-current-buffer buffer
+      (goto-char (point-max))
+      (unless (bobp) (insert "\n"))
+      (backward-list 1)
+      (--> (read buffer)
+          (plist-put it :stop (format-time-string "%FT%T%z"))
+          (progn
+            (backward-list 1)
+            (chronometrist-delete-list)
+            (plist-pp it buffer)))
+      (save-buffer))))
 
 (provide 'chronometrist-sexp)
 
