8eaa6c40a571b547efc859ff517536a74e8a03a8
contrapunctus <music.kashish@gmail.com>
2020-04-15 12:40:32 +0530

    Implement timed alerts

--- chronometrist-targets.el
+++ chronometrist-targets.el
@@ -42,7 +42,43 @@ If TARGETS-LIST is not supplied, `chronometrist-time-targets-list' is used."
              when (member task list)
              return (car list))))
 
-(defun chronometrist-run-notification-timers ())
+(defvar chronometrist-approach-timer nil)
+(defvar chronometrist-complete-timer nil)
+(defvar chronometrist-exceed-timer nil)
+
+(defun chronometrist-approach-alert (task)
+  (alert (format "5 minutes remain for %s" (downcase task))))
+
+(defun chronometrist-complete-alert (task)
+  (alert (format "Target for %s reached" (downcase task))))
+
+(defun chronometrist-exceed-alert (task)
+  (alert (format "You are exceeding the time for %s!" (downcase task))
+         :severity 'high))
+
+(defun chronometrist-minutes-string (minutes)
+  (format "%s minutes" minutes))
+
+(defun chronometrist-run-alert-timers (task)
+  "Run timers to alert the user of the time spent on TASK.
+Add this to `chronometrist-after-in-functions' to use."
+  (let ((target (chronometrist-get-target task)))
+    (when target ;; don't run for tasks which don't have a target defined
+      (setq chronometrist-approach-timer
+            (run-at-time (chronometrist-minutes-string (- target 5))
+                         nil
+                         #'chronometrist-approach-alert
+                         task)
+            chronometrist-complete-timer
+            (run-at-time (chronometrist-minutes-string target)
+                         nil
+                         #'chronometrist-complete-alert
+                         task)
+            chronometrist-exceed-timer
+            (run-at-time (chronometrist-minutes-string (+ target 5))
+                         nil
+                         #'chronometrist-exceed-alert
+                         task)))))
 
 (provide 'chronometrist-targets)
 
