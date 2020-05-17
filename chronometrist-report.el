contrapunctus-1/chronometrist

commit 7bf6750c21f88ae9f53a9ba324ac6ecb36f3935d

Migrate report to use ts.el

chronometrist-report.el
@@ -57,16 +57,11 @@ information (see (info \"(elisp)Time Conversion\"))."
 
 (defun chronometrist-report-date->dates-in-week (first-date-in-week)
   "Return a list of dates in a week, starting from FIRST-DATE-IN-WEEK.
-Each day is a time value (see (info \"(elisp)Time of Day\")).
+Each date is a ts struct (see `ts.el').
 
-FIRST-DATE-IN-WEEK must be a time value representing the first date."
-  (--> '(0 1 2 3 4 5 6)
-       ;; 1 day = 86400 seconds
-       (--map (* 86400 it) it)
-       (--map (list
-               (car first-date-in-week)
-               (+ (cadr first-date-in-week) it))
-              it)))
+FIRST-DATE-IN-WEEK must be a ts struct representing the first date."
+  (cl-loop for i from 0 to 6 collect
+           (ts-adjust 'day i first-date-in-week)))
 
 (defun chronometrist-report-date->week-dates ()
   "Return dates in week as a list.
@@ -81,7 +76,7 @@ The first date is the first occurrence of
 
 (defun chronometrist-report-entries ()
   "Create entries to be displayed in the `chronometrist-report' buffer."
-  (let* ((week-dates        (chronometrist-report-date->week-dates))) ;; uses today if chronometrist-report--ui-date is nil
+  (let* ((week-dates (chronometrist-report-date->week-dates))) ;; uses today if chronometrist-report--ui-date is nil
     (setq chronometrist-report--ui-week-dates week-dates)
     (mapcar (lambda (task)
               (let ((task-daily-time-list
@@ -126,7 +121,8 @@ If FIRSTONLY is non-nil, insert only the first keybinding found."
         (w "\n    "))
     (goto-char (point-min))
     (insert "                         ")
-    (insert (mapconcat #'chronometrist-date
+    (insert (mapconcat (lambda (ts)
+                         (ts-format "%F" ts))
                        (chronometrist-report-date->week-dates)
                        " "))
     (insert "\n")
@@ -270,12 +266,10 @@ With prefix argument ARG, move back ARG weeks."
                  (abs arg)
                1)))
     (setq chronometrist-report--ui-date
-          (thread-first (if chronometrist-report--ui-date
-                            (parse-iso8601-time-string
-                             (chronometrist-iso-date->timestamp chronometrist-report--ui-date))
-                          (current-time))
-            (time-subtract `(0 ,(* 7 arg 86400)))
-            (chronometrist-date))))
+          (ts-adjust 'day -7
+                     (if chronometrist-report--ui-date
+                         chronometrist-report--ui-date
+                       (ts-now)))))
   (setq chronometrist-report--point (point))
   (kill-buffer)
   (chronometrist-report t))
@@ -288,12 +282,10 @@ With prefix argument ARG, move forward ARG weeks."
                  (abs arg)
                1)))
     (setq chronometrist-report--ui-date
-          (thread-first (if chronometrist-report--ui-date
-                            (parse-iso8601-time-string
-                             (chronometrist-iso-date->timestamp chronometrist-report--ui-date))
-                          (current-time))
-            (time-add `(0 ,(* 7 arg 86400)))
-            (chronometrist-date)))
+          (ts-adjust 'day 7
+                     (if chronometrist-report--ui-date
+                         chronometrist-report--ui-date
+                       (ts-now))))
     (setq chronometrist-report--point (point))
     (kill-buffer)
     (chronometrist-report t)))

chronometrist-time.el
@@ -48,7 +48,7 @@ DATE should be an ISO-8601 date string (\"YYYY-MM-DD\")."
 
 (cl-defun chronometrist-date (&optional (ts (ts-now)))
   "Return a ts struct representing the time 00:00:00 on today's date.
-If TS is supplied, use the date from that instead of today.
+If TS is supplied, use that date instead of today.
 TS should be a ts struct (see `ts.el')."
   (ts-apply :hour 0 :minute 0 :second 0 ts))
 

doc/manual.org
@@ -173,6 +173,7 @@
     3. Internal Variable - chronometrist-report--point
     4. Function - chronometrist-report-date ()
     5. Function - chronometrist-report-date->dates-in-week (first-date-in-week)
+       * ts-1 -> (ts-1 ... ts-7)
     6. Function - chronometrist-report-date->week-dates ()
     7. Function - chronometrist-report-entries ()
     8. Function - chronometrist-report-format-date (format-string time-date)

commit e04cb19be692dc30847cd390db134c06d1d1672b

Fix previous/next scrolling bug introduced in 7bf6750

chronometrist-report.el
@@ -266,7 +266,7 @@ With prefix argument ARG, move back ARG weeks."
                  (abs arg)
                1)))
     (setq chronometrist-report--ui-date
-          (ts-adjust 'day -7
+          (ts-adjust 'day (- (* arg 7))
                      (if chronometrist-report--ui-date
                          chronometrist-report--ui-date
                        (ts-now)))))
@@ -282,7 +282,7 @@ With prefix argument ARG, move forward ARG weeks."
                  (abs arg)
                1)))
     (setq chronometrist-report--ui-date
-          (ts-adjust 'day 7
+          (ts-adjust 'day (* arg 7)
                      (if chronometrist-report--ui-date
                          chronometrist-report--ui-date
                        (ts-now))))

chronometrist-statistics.el
@@ -283,7 +283,7 @@ If ARG is a numeric argument, go back that many times."
          (start (plist-get chronometrist-statistics--ui-state :start)))
     (cl-case (plist-get chronometrist-statistics--ui-state :mode)
       ('week
-       (let* ((new-start (ts-adjust 'day -7 start))
+       (let* ((new-start (ts-adjust 'day (- (* arg 7)) start))
               (new-end   (ts-adjust 'day +6 new-start)))
          (plist-put chronometrist-statistics--ui-state :start new-start)
          (plist-put chronometrist-statistics--ui-state :end   new-end))))
@@ -302,7 +302,7 @@ If ARG is a numeric argument, go forward that many times."
          (start (plist-get chronometrist-statistics--ui-state :start)))
     (cl-case (plist-get chronometrist-statistics--ui-state :mode)
       ('week
-       (let* ((new-start (ts-adjust 'day 7 start))
+       (let* ((new-start (ts-adjust 'day (* arg 7) start))
               (new-end   (ts-adjust 'day 6 new-start)))
          (plist-put chronometrist-statistics--ui-state :start new-start)
          (plist-put chronometrist-statistics--ui-state :end   new-end))))