736eab7ae4f0bf1445b6913d525b90b4640b6565
contrapunctus <contrapunctus@disroot.org>
2020-11-04 16:34:50 +0530

    Remove old definition of chronometrist-plist-pp-buffer

--- elisp/chronometrist-plist-pp.el
+++ elisp/chronometrist-plist-pp.el
@@ -121,54 +121,6 @@ that point is after the first opening parenthesis."
     (when (bolp) (delete-char -1))
     (up-list)))
 
-(cl-defun chronometrist-plist-pp-buffer (&optional (left-indent 0) in-sublist-p)
-  "Naive pretty-printer for plists."
-  (let (right-indent sexp)
-    (goto-char (point-min))
-    (while (not (eobp))
-      (unless (looking-at-p ")")
-        (setq sexp (save-excursion (read (current-buffer)))))
-      (cond
-       ((and (bobp) (json-plist-p sexp))
-        ;; first keyword-value pair
-        (setq right-indent (chronometrist-plist-pp-longest-keyword-length))
-        (ignore-errors (down-list 1))
-        (setq sexp (save-excursion (read (current-buffer))))
-        ;; the first keyword does not need to be left-indented
-        (insert (chronometrist-plist-pp-indent-sexp sexp :right-indent right-indent))
-        (forward-sexp 1))
-       ((looking-at-p ")")
-        (if (bolp)
-            (progn (delete-char -1) (forward-char 1))
-          (forward-char))
-        (setq in-sublist-p nil))
-       ;; ((looking-at chronometrist-plist-pp-whitespace-re)
-       ;;  (delete-region (match-beginning 0) (match-end 0)))
-       ;; any other keyword
-       ((keywordp sexp)
-        (insert "\n " (chronometrist-plist-pp-indent-sexp sexp :left-indent left-indent
-                                    :right-indent right-indent)))
-       ;; we are before a list as a value
-       ;; FIXME - not using (looking-at-p "(") results in extra
-       ;; iterations; but that needs forward-char rather than
-       ;; forward-sexp as the fallback
-       ((listp sexp)
-        ;; TODO - fix indentation
-        (let ((sublist-indent (if in-sublist-p left-indent (chronometrist-plist-pp-column))))
-          (cond ((json-plist-p sexp)
-                 (chronometrist-sexp-delete-list)
-                 (insert (chronometrist-plist-pp-to-string sexp sublist-indent))
-                 (insert "\n"))
-                ((chronometrist-plist-pp-alist-p sexp)
-                 (down-list)
-                 (while (not (looking-at-p ")"))
-                   (forward-sexp)
-                   (insert "\n")))
-                (t (down-list)
-                   (setq in-sublist-p t
-                         left-indent (chronometrist-plist-pp-column))))))
-       (t (forward-sexp))))))
-
 (defun chronometrist-plist-pp-to-string (object &optional left-indent)
   "Convert OBJECT to a pretty-printed string."
   (with-temp-buffer
