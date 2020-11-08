contrapunctus-1/chronometrist

commit 6b1d55493dbff471c6c330f4c5c715fa1d724a35

Remove ->>

elisp/chronometrist-sexp.el
@@ -69,8 +69,7 @@ were none."
                                   (setq pending-expr (cl-second split-expr))
                                   (cl-first split-expr))
                                  (t expr)))
-               (new-value-date (->> (plist-get new-value :start)
-                                    (s-left 10)))
+               (new-value-date (plist-get new-value :start (s-left 10)))
                (existing-value (gethash new-value-date chronometrist-events)))
           (unless pending-expr (cl-incf index))
           (puthash new-value-date

commit 9c503a88eb06a67277f84760261433fe100036f4

Fix error introduced in 6b1d554

elisp/chronometrist-sexp.el
@@ -69,7 +69,7 @@ were none."
                                   (setq pending-expr (cl-second split-expr))
                                   (cl-first split-expr))
                                  (t expr)))
-               (new-value-date (plist-get new-value :start (s-left 10)))
+               (new-value-date (s-left 10 (plist-get new-value :start)))
                (existing-value (gethash new-value-date chronometrist-events)))
           (unless pending-expr (cl-incf index))
           (puthash new-value-date