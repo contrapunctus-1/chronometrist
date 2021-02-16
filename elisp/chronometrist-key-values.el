f1632ef4b9d03d062f8b5a55b4efab80c19fa68a
contrapunctus <contrapunctus@disroot.org>
2021-02-16 15:35:32 +0530

    [key-values] add library headers

--- elisp/chronometrist-key-values.el
+++ elisp/chronometrist-key-values.el
@@ -1,5 +1,12 @@
 ;;; chronometrist-key-values.el --- add key-values to Chronometrist data -*- lexical-binding: t; -*-
 
+;; Author: contrapunctus <xmpp:contrapunctus@jabber.fr>
+;; Maintainer: contrapunctus <xmpp:contrapunctus@jabber.fr>
+;; Keywords: calendar
+;; Homepage: gemini://tilde.team/~contrapunctus/software.gmi
+;; Package-Requires: ((chronometrist "0.5.0") (choice "0.0.1"))
+;; Version: 0.1.0
+
 ;; This is free and unencumbered software released into the public domain.
 ;;
 ;; Anyone is free to copy, modify, publish, use, compile, sell, or
